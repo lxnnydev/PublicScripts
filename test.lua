135b801e144f794964be9e87fa2310fb8364c5a7
lxnny <89148963+lxnnydev@users.noreply.github.com>
2023-01-03 14:53:05 +0100

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,28 +1,46 @@
 local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
 
-local win = Flux:Window("PREVIEW", "Baseplate", Color3.fromRGB(255, 110, 48), Enum.KeyCode.LeftControl)
-local tab = win:Tab("Tab 1", "http://www.roblox.com/asset/?id=6023426915")
-tab:Button("Kill all", "This function may not work sometimes and you can get banned.", function()
-Flux:Notification("Killed all players successfully!", "Alright")
+local win = Flux:Window("DELTA EXPLOITS", "Blox Fruits", Color3.fromRGB(76, 82, 255), Enum.KeyCode.LeftControl)
+local tab = win:Tab("Blox Fruits", "http://www.roblox.com/asset/?id=6023426915")
+
+tab:Label("Hoho Hub (No Key)")
+tab:Button("Execute Script", "This executes Hoho Hub", function()
+Flux:Notification("Sucessfully executed script!", "OK!")
 end)
-tab:Label("This is just a label.")
+
 tab:Line()
-tab:Toggle("Auto-Farm Coins", "Automatically collects coins for you!", function(t)
-print(t)
+
+tab:Label("Mango Hub (No Key)")
+tab:Button("Execute Script", "This executes Mango Hub", function()
+Flux:Notification("Sucessfully executed script!", "OK!")
 end)
-tab:Slider("Walkspeed", "Makes your faster.", 0, 100,16,function(t)
-print(t)
+
+tab:Line()
+
+tab:Label("Zen Hub (Has Key)")
+tab:Button("Execute Script", "This executes Zen Hub", function()
+Flux:Notification("Sucessfully executed script!", "OK!")
 end)
-tab:Dropdown("Part to aim at", {"Torso","Head","Penis"}, function(t)
-print(t)
+
+tab:Line()
+
+tab:Label("Mukuro Hub (Has Key)")
+tab:Button("Execute Script", "This executes Mukuro Hub", function()
+Flux:Notification("Sucessfully executed script!", "OK!")
 end)
-tab:Colorpicker("ESP Color", Color3.fromRGB(255,1,1), function(t)
-print(t)
+
+tab:Line()
+
+tab:Label("Table Hub (Has Key)")
+tab:Button("Execute Script", "This executes Table Hub", function()
+Flux:Notification("Sucessfully executed script!", "OK!")
 end)
-tab:Textbox("Gun Power", "This textbox changes your gun power, so you can kill everyone faster and easier.", true, function(t)
-print(t)
+
+tab:Line()
+
+tab:Label("Neva Hub (No Key)")
+tab:Button("Execute Script", "This executes Neva Hub", function()
+Flux:Notification("Sucessfully executed script!", "OK!")
 end)
-tab:Bind("Kill Bind", Enum.KeyCode.Q, function()
-print("Killed a random person!")
-end)
-win:Tab("Tab 2", "http://www.roblox.com/asset/?id=6022668888")
+
+tab:Line()
