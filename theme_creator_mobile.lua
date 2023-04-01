38374c1e644b0076c0c71fcae9dbefe83a1927cc
lxnny <89148963+lxnnydev@users.noreply.github.com>
2023-04-01 21:52:46 +0200

    Update theme_creator_mobile.lua

--- theme_creator_mobile.lua
+++ theme_creator_mobile.lua
@@ -25,6 +25,37 @@ game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Playe
 
 end)
 
+Section:NewButton("Share Theme", "Share your Theme", function()
+    local scriptString = [[
+    local customtheme = [[
+        {
+            "BgColor": "]]..BgColor..[[",
+            "BtnColor": "]]..BtnColor..[[",
+            "MenuColor": "]]..MenuColor..[[",
+            "SettingInputColor": "]]..SettingBtnColor..[[" 
+        }
+		]].."]]"..[[ 
+
+    writefile("theme.delta", customtheme)
+    game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)
+]]
+
+setclipboard(scriptString)
+game.StarterGui:SetCore("SendNotification", 
+            {
+            Title = "Delta Android";
+            Text = "Your theme is now a script in your clipboard!";
+            Duration = 1;
+            })
+
+end)
+
+Section:NewButton("Reset Theme/Normal Theme", "Deletes your theme", function()
+delfile("theme.delta")
+wait()
+game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)
+end)
+
 Section:NewColorPicker("Background Color", "Color Info", Color3.fromHex("#141417"), function(color)
     BgColor = color:ToHex()
 end)
@@ -40,4 +71,3 @@ end)
 Section:NewColorPicker("Settings Input Color", "Color Info", Color3.fromHex("#1E1E1E"), function(color)
     SettingBtnColor = color:ToHex()
 end)
-
